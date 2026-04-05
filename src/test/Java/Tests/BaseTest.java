69080f9547c05f28341c63840e33136e5aa03dfd
xSkat <19041996zx@mail.ru>
2026-04-05 14:48:27 +0300

    rename javaclass3

---
+++ src/test/Java/Tests/BaseTest.java
@@ -0,0 +1,13 @@
+package Tests;
+
+import org.junit.jupiter.api.BeforeAll;
+
+import java.lang.module.Configuration;
+
+public class BaseTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.
+
+    }
+}
--- src/test/Java/Tests/HomeWorkTests.java
+++ src/test/Java/Tests/HomeWorkTests.java
@@ -2,6 +2,8 @@ package Tests;
 
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class HomeWorkTests {
@@ -9,5 +11,21 @@ public class HomeWorkTests {
     @Test
     void firststep() {
         open("https://demoqa.com/automation-practice-form");
+        $("").val("");
+        $("").val("");
+        $("").val("");
+        $("").val("");
+        $("").val("");
+        $("").val("");
+        $("").val("");
+        $("").val("");
+        $("").click();
+
+        $("").shouldHave(text(""));
+        $("").shouldHave(text(""));
+        $("").shouldHave(text(""));
+        $("").shouldHave(text(""));
+        $("").shouldHave(text(""));
+
     }
 }
