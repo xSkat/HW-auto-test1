d12a0e119d2e0fe556c51042a84ae254f0e246b4
xSkat <19041996zx@mail.ru>
2026-04-05 16:46:22 +0300

    50% test

--- src/test/Java/Tests/HomeWorkTests.java
+++ src/test/Java/Tests/HomeWorkTests.java
@@ -3,23 +3,29 @@ package Tests;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
 
 public class HomeWorkTests extends BaseTest {
 
     @Test
     void firststep() {
         open("/automation-practice-form");
-        $("").val("");
-        $("").val("");
-        $("").val("");
-        $("").val("");
-        $("").val("");
-        $("").val("");
-        $("").val("");
-        $("").val("");
-        $("").click();
+        $("#firstName").val("Tony");
+        $("#lastName").val("Kek");
+        $("#userEmail").val("zdarova@gmail.com");
+        $("#gender-radio-1").click();
+        $("#userNumber").val("asdasd");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("May");
+        $(".react-datepicker__year-select").selectOption("1968");
+        $(".react-datepicker__day--014").click();
+        $("#subjectsInput").val("history").pressEnter();;
+        $("#hobbies-checkbox-2").click();
+        $("[]").val("");
+        $("[]").val("");
+        $("[id=gender-radio-1]").click();
 
         $("").shouldHave(text(""));
         $("").shouldHave(text(""));
